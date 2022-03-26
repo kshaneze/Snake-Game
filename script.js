4ae42bb7fc9c5fe5ef33451a97a27b6526495482
niksa <niksaopfor@gmail.com>
2022-03-26 15:15:20 +0100

    Working on app

--- script.js
+++ script.js
@@ -21,11 +21,13 @@ let dy = 0;
 let changing_direction = false;
 let foodX;
 let foodY;
+
 const snakeBoard = document.getElementById('canvas');
 const snakeBoardCtx = snakeBoard.getContext('2d');
 snakeBoard.width = 800;
 snakeBoard.height = 600;
 
+/////////////////////////////////////////////////
 // creating and clearing the board
 const clearBoard = function () {
   snakeBoardCtx.fillStyle = board_background;
@@ -33,7 +35,7 @@ const clearBoard = function () {
   snakeBoardCtx.fillRect(0, 0, snakeBoard.width, snakeBoard.height);
   snakeBoardCtx.strokeRect(0, 0, snakeBoard.width, snakeBoard.height);
 };
-
+/////////////////////////////////////////////////
 // Main funciton where everyginh will go
 const main = function () {
   changing_direction = false;
@@ -49,7 +51,7 @@ const main = function () {
   }, 100);
   document.addEventListener('keydown', changeDirection);
 };
-
+/////////////////////////////////////////////////
 // drawing part for each element in array
 const drawSnakePart = function (snakePart) {
   snakeBoardCtx.fillStyle = snake_col;
@@ -59,12 +61,13 @@ const drawSnakePart = function (snakePart) {
   snakeBoardCtx.strokeRect(snakePart.x, snakePart.y, 10, 10);
   snakeBoardCtx.closePath();
 };
-
+/////////////////////////////////////////////////
 // Function that loops over snake array and create snake
 const drawSnake = function () {
   snake.forEach(drawSnakePart);
 };
 
+/////////////////////////////////////////////////
 // Game Over
 const checkIfGameOver = function () {
   // if snake posotion is at the end
@@ -82,6 +85,8 @@ const checkIfGameOver = function () {
   }
 };
 
+/////////////////////////////////////////////////
+// Chaning direction on pressed keys
 const changeDirection = function (event) {
   const LeftKey = 37;
   const RightKey = 39;
@@ -115,14 +120,22 @@ const changeDirection = function (event) {
   }
 };
 
+/////////////////////////////////////////////////
 // Moving the snake automatically
 
 const snakeMovement = function () {
   const head = { x: snake[0].x + dx, y: snake[0].y + dy };
   snake.unshift(head);
-  snake.pop();
+
+  const hasEatenFood = snake[0].x === foodX && snake[0].y === foodY;
+  if (hasEatenFood) {
+    generateFood();
+  } else {
+    snake.pop();
+  }
 };
 
+///////////////////////////////////////////////
 // Creating food at random places
 const randomFood = function (min, max) {
   return Math.round((Math.random() * (max - min) + min) / 10) * 10;
@@ -144,5 +157,9 @@ const drawFood = function () {
   snakeBoardCtx.fillRect(foodX, foodY, 10, 10);
   snakeBoardCtx.strokeRect(foodX, foodY, 10, 10);
 };
+
+/////////////////////////////////////////////////
+// Eating the food and adding to snake array
+
 generateFood();
 main();
